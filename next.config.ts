ddbb9c82d3881418b90b4852da0f766db7690ab7
Karl-Maker <karljohanbailey98@gmail.com>
2025-08-21 11:57:06 -0400

    fix

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   // Optimize for Netlify deployment
   output: 'standalone',
-  experimental: {
-    // Reduce bundle size
-    optimizeCss: true,
-  },
   // Disable telemetry
   telemetry: false,
 };
